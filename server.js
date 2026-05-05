0f468a1eb7cb6719884a71c28993d13fd60626cd
dk3nn <kennaleydavin@gmail.com>
2026-05-05 15:32:37 -0400

    Remove static file serving for production
    Remove catch-all route for production environment.

--- server.js
+++ server.js
@@ -231,9 +231,6 @@ app.delete('/saved/:id', async (req,res) => {
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/dist')));
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
-    });
 }
 
 app.listen(PORT, () => {
