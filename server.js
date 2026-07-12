e57fd8241417907b0b83d34e6bb70f4158bbdc14
Davin Kennaley <kennaleydavin@gmail.com>
2026-07-12 12:49:24 -0400

    1

--- server.js
+++ server.js
@@ -237,7 +237,7 @@ if (process.env.NODE_ENV === 'production') {
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/', 'index.html'));
 });
 
 app.listen(PORT, () => {
