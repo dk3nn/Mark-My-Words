bfd3ad327a6f394c9158456c4ad76b853e169780
Davin Kennaley <kennaleydavin@gmail.com>
2026-07-12 12:52:39 -0400

    1

--- server.js
+++ server.js
@@ -234,7 +234,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/dist')));
 }
 */
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, 'client/', 'index.html'));
